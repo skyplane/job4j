cc7bbfce3b2a99dd48e33aee9583d251f322f9d1
Vladimir Kovalenko <mailto:skyplane.ru@gmail.com>
2018-06-15 16:21:51 +0700

    1.3. JavDoc. Code convetion.

--- chapter_001/src/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/ru/job4j/calculate/Calculate.java
@@ -1,9 +1,19 @@
 package ru.job4j.calculate;
 
+/*
+@version 1.0
+@author Vladimir Kovalenko (skyplane.ru@gmail.com)
+@since 15.06.2018
+*/
+
+
 
 public class Calculate {
 
 
+	/* Конструктор, вывод строки в консоль.
+	@param arguments - String[] args
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
