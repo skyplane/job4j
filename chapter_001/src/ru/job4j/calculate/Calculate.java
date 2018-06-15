38040580d179722d81c0c72105dd08c7d8616653
Vladimir Kovalenko <mailto:skyplane.ru@gmail.com>
2018-06-15 16:30:40 +0700

    1.3.1 Maven Plugins

--- chapter_001/src/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/ru/job4j/calculate/Calculate.java
@@ -1,9 +1,9 @@
 package ru.job4j.calculate;
 
-/*
-@version 1.0
-@author Vladimir Kovalenko (skyplane.ru@gmail.com)
-@since 15.06.2018
+/**
+*@version 1.0
+*@author Vladimir Kovalenko (skyplane.ru@gmail.com)
+*@since 15.06.2018
 */
 
 
@@ -12,7 +12,7 @@ public class Calculate {
 
 
 	/* Конструктор, вывод строки в консоль.
-	@param arguments - String[] args
+	*@param arguments - String[] args
 	*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
