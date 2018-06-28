d19bd942129811bb1f8e06b8641891b79425cfe3
Vladimir Kovalenko <mailto:skyplane.ru@gmail.com>
2018-06-28 16:25:48 +0700

    3. Реализовать коллекцию типа Set на базе хэш-таблицы [#998]

---
+++ chapter_001/src/ru/job4j/set/SimpleHashSet.java
@@ -0,0 +1,117 @@
+package ru.job4j.set;
+
+import javafx.util.Pair;
+import ru.job4j.generics.SimpleArray;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Простая реализация хеш-таблицы
+ *
+ * @author Vladimir Kovalenko (mailto:skyplane.ru@gmail.com)
+ * @version $
+ * @since 0.1
+ */
+
+public class SimpleHashSet<T> {
+
+
+    private Pair<Integer, T>[] values;
+    private int index = 0;
+    private int containerSize;
+
+
+    public SimpleHashSet(int size) {
+        this.values = new Pair[size];
+        containerSize = size;
+    }
+
+
+    private final double PHI = (1 + Math.sqrt(5)) / 2;
+
+    private int findNearestIndexWithExistCheck(int hash, boolean exist) {
+        int result = 0;
+        if (size() > 0) {
+            int x1, x2;
+            int a = 0;
+            int b = size() - 1;
+            if (hash > values[size() - 1].getKey())
+                result = size();
+            else
+                while (true) {
+                    x1 = (int) Math.round(values[b].getKey() - (values[b].getKey() - values[a].getKey()) / PHI);
+                    x2 = (int) Math.round(values[a].getKey() + (values[b].getKey() - values[a].getKey()) / PHI);
+                    if (Math.abs(x1 - hash) > Math.abs(x2 - hash))
+                        a = (int) Math.round(b - (b - a) / PHI);
+                    else
+                        b = (int) Math.round(a + (b - a) / PHI);
+                    ;
+                    if (Math.abs(b - a) < 2) {
+                        if (values[a].getKey().equals(hash)) {
+                            result = exist ? a : -1;
+                            break;
+                        }
+                        if (values[b].getKey().equals(hash)) {
+                            result = exist ? b : -1;
+                            break;
+                        }
+                        result = exist ? -1 : Math.max(a, b);
+                        break;
+                    }
+                }
+        }
+        return result;
+    }
+
+    public boolean contains(T model) {
+        return findNearestIndexWithExistCheck(model.hashCode(), true) >= 0;
+    }
+
+    public boolean add(T model) {
+        int position = findNearestIndexWithExistCheck(model.hashCode(), false);
+        if (position >= 0) {
+            if (index >= containerSize) {
+                if (containerSize > Integer.MAX_VALUE / 2) {
+                    throw new OutOfMemoryError();
+                }
+                containerSize = containerSize * 2;
+                values = Arrays.copyOf(values, containerSize);
+            }
+
+            if (position < index)
+                System.arraycopy(
+                        values, position,
+                        values, position + 1, this.index - position);
+            if (size() > 0)
+                values[position] = new Pair<>(model.hashCode(), model);
+            else
+                values[position] = new Pair<>(model.hashCode(), model);
+            index++;
+        }
+        return position >= 0;
+    }
+
+    public boolean remove(T model) {
+        int position = findNearestIndexWithExistCheck(model.hashCode(), true);
+        if (position >= 0) {
+            System.arraycopy(
+                    values, position + 1,
+                    values, position, this.index - position - 1);
+            index--;
+        }
+        return position >= 0;
+    }
+
+
+    public int size() {
+        return this.index;
+    }
+
+
+}
+
+
+
+
---
+++ chapter_001/test/java/ru/job4j/set/SimpleHashSetTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Тесты для простой хеш-таблицы
+ *
+ * @author Vladimir Kovalenko (mailto:skyplane.ru@gmail.com)
+ * @version $
+ * @since 0.1
+ */
+
+public class SimpleHashSetTest {
+
+
+    private SimpleHashSet<Integer> set;
+
+    @Before
+    public void beforeTest() {
+        set = new SimpleHashSet<>(3);
+
+    }
+
+    @Test
+    public void shouldSetExistTwoElements() {
+        assertThat(set.add(2), is(true));
+        assertThat(set.add(1), is(true));
+        assertThat(set.add(3), is(true));
+        assertThat(set.add(2), is(false));
+        assertThat(set.add(4), is(true));
+        assertThat(set.add(5), is(true));
+        assertThat(set.add(7), is(true));
+        assertThat(set.remove(2), is(true));
+        assertThat(set.add(6), is(true));
+        assertThat(set.contains(3), is(true));
+        assertThat(set.contains(2), is(false));
+    }
+
+
+
+
+}
