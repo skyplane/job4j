1f3bce623a7fb16e0e1376d6c2bcc190322863e7
Vladimir Kovalenko <mailto:skyplane.ru@gmail.com>
2018-06-29 16:18:12 +0700

    2. Не перекрывать equals hashCode [#1005]

---
+++ chapter_001/test/java/ru/job4j/map/MapTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.map;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * tests for map
+ *
+ * @author Vladimir Kovalenko (mailto:skyplane.ru@gmail.com)
+ * @version $
+ * @since 0.1
+ */
+
+public class MapTest {
+
+
+    @Ignore
+    @Test
+    public void testForMap(){
+        User user1 = new User("name", 1, new GregorianCalendar(1,1,1));
+        User user2 = new User("name", 1, new GregorianCalendar(1,1,1));
+
+        Map<User, Object> map = new HashMap<>();
+
+        map.put(user1,"first");
+        map.put(user2,"second");
+
+        System.out.println(map);
+
+
+    }
+
+
+}
